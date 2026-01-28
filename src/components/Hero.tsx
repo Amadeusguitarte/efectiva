8ee108671ef9817e21d0d90642c0d2608fdd34a5
Amadeus Arteaga <amadeusguitarte@gmail.com>
2026-01-28 18:16:00 -0500

    style(Hero): soften bottom gradient and match next section background color for seamless transition

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -59,7 +59,7 @@ export const Hero = () => {
       </div>
 
       {/* Bottom Fade to White */}
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10" />
+      <div className="absolute bottom-0 left-0 right-0 h-64 bg-gradient-to-t from-[#f8f9fc] via-[#f8f9fc]/80 to-transparent z-10" />
     </section>
   );
 };
